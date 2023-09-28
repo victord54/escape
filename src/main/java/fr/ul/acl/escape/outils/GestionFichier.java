4e7fbdf44358ae6a69abe3c9fdf6d97af43b2cf8
Demange Dan <demange.danodilon@gmail.com>
2023-09-28 11:23:44 +0200

    GestionFichier v1.0 fidèle au diagramme. Facilement évolutif si besoin qu'il renvoie une ArrayList ou un tableau 2D.
    Le constructeur actuellement vide : la classe ne possède que des fonction indépendantes

--- src/main/java/fr/ul/acl/escape/outils/GestionFichier.java
+++ src/main/java/fr/ul/acl/escape/outils/GestionFichier.java
@@ -1,7 +1,57 @@
 package fr.ul.acl.escape.outils;
 
+import java.io.*;
+
 public class GestionFichier {
+
+    /*
+    Constructeur pour moi inutile car les fonctions de gestion de fichier seront toutes indépendantes
+     */
+    public GestionFichier(){}
+
+    /*
+    Fonction qui renvoie un tableau de char du contenu du fichier
+     */
     public char[] lireFichierCarte(String nomFichier){
+
+        char [] tableau;
+
+        try
+        {
+            // Le fichier d'entrée se trouve dans le dossier cartes des ressources
+            File file = new File("./src/main/ressources/cartes/"+nomFichier+".txt");
+
+            // Créer l'objet File Reader qui permet de lire le fichier texte
+            FileReader fileReader = new FileReader(file);
+
+            // Créer l'objet BufferedReader qui récupère les informations du file reader
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            // Créer l'objet stringbuffer qui stocke les caractères luent
+            StringBuilder stringBuilder = new StringBuilder();
+
+            // Actuellement, le fichier texte est lu ligne par ligne ce qui peut permettre de facilement modifier le code
+            // Pour qu'il renvoie un tableau à 2 dimensions.
+            String line;
+            while((line = bufferedReader.readLine()) != null)
+            {
+                // ajoute la ligne au buffer
+                stringBuilder.append(line);
+                stringBuilder.append("\n");
+            }
+            fileReader.close();
+
+            // On rempli l'objet tableau avec le contenu du fichier texte
+            tableau = new char[stringBuilder.length()];
+            for (int i = 0; i < stringBuilder.length(); i++) {
+                tableau[i] = stringBuilder.charAt(i);
+            }
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
         return null;
     }
 }

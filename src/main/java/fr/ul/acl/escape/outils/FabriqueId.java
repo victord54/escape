5b53c7276664f4437c0dc4adb400b513abfff72c
Victor Dallé <victor.dalle8@etu.univ-lorraine.fr>
2023-10-27 10:14:15 +0200

    Fabrique identifiants

--- src/main/java/fr/ul/acl/escape/outils/FabriqueId.java
+++ src/main/java/fr/ul/acl/escape/outils/FabriqueId.java
@@ -2,12 +2,19 @@ package fr.ul.acl.escape.outils;
 
 public class FabriqueId {
     private static FabriqueId instance;
+    private int id;
 
     private FabriqueId() {
-
+        id = 0;
     }
 
     public static FabriqueId getInstance() {
+        if (instance == null) instance = new FabriqueId();
         return instance;
+    }
+
+    public int getId() {
+        id++;
+        return id;
     }
 }

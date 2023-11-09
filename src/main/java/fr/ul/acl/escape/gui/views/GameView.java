95a517b19e8c8dfa5569ca1ec37a6d30a43f3f46
clairekth <claire.kurth@orange.fr>
2023-11-09 17:22:48 +0100

    Affiche des coeurs actuels du personnage sur l'overlay

--- src/main/java/fr/ul/acl/escape/gui/views/GameView.java
+++ src/main/java/fr/ul/acl/escape/gui/views/GameView.java
@@ -178,6 +178,50 @@ public class GameView extends View implements GameInterface {
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.setFill(Color.LIGHTGREEN);
         gc.fillText("FPS: " + engine.getFPS(), 10, canvas.getHeight() - 10);
+
+        // number of hearts the hero currently has
+        double coeurs = this.gameController.getHeros().getCoeurs();
+        // number max of hearts the hero can have
+        double coeursMax = Donnees.HERO_HEART;
+        // number of lost hearts
+        double coeursPerdu = coeursMax - coeurs;
+        // number of full hearts remaining
+        int nbCoeursRestantPleins = (int) (coeurs);
+        // quartile of heats remaining (0.75,0.5,0.25)
+        double coeursRestantsNonPleins = coeurs - nbCoeursRestantPleins;
+        // number of lost hearts full
+        int coeursPerduPleins = (int) coeursPerdu ;
+
+        int deplacement = 0;
+
+        // draw full heart
+        for (int i = 0; i < nbCoeursRestantPleins ; i ++){
+            gc.setFill(Color.RED);
+            gc.fillRect(10+(deplacement*30), 10, 25, 25);
+            deplacement ++;
+        }
+
+        // draw if there is a heart not full
+        if (coeursRestantsNonPleins == 0.75){
+            gc.setFill(Color.BLUEVIOLET);
+            gc.fillRect(10+(deplacement*30), 10, 25, 25);
+            deplacement ++;
+        } else if (coeursRestantsNonPleins == 0.5){
+            gc.setFill(Color.BLUE);
+            gc.fillRect(10+(deplacement*30), 10, 25, 25);
+            deplacement++;
+        } else if (coeursRestantsNonPleins == 0.25){
+            gc.setFill(Color.ORANGE);
+            gc.fillRect(10+(deplacement*30), 10, 25, 25);
+            deplacement++;
+        }
+
+        // draw the lost hearts
+        for (int i = 0 ; i < coeursPerduPleins ; i ++){
+            gc.setFill(Color.PINK);
+            gc.fillRect(10+(deplacement*30), 10, 25, 25);
+            deplacement ++;
+        }
     }
 
     /**
